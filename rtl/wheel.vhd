b450a8909d872f9243615a0e6909e4b4bc03030f
sjeapes@gmail.com <sjeapes@gmail.com@1787d804-c95a-1144-e8a0-77df1a188300>
2013-07-25 20:18:28 +0000

    Initial Creation

---
+++ rtl/wheel.vhd
@@ -0,0 +1,66 @@
+--Filename: wheel.vhd
+--
+--Description: 
+-- Performs the actions of the wheels
+--
+--
+--    Copyright (C) 2013 Stephen Jeapes (vhdl-enigma@arcoarena.co.uk)
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--Wiring for wheels taken from:
+-- http://www.ellsbury.com/ultraenigmawirings.htm
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.letters_pack.all;
+
+
+entity wheel is
+
+   port 
+   (
+      clk_in   :  in std_logic; --! Clock input signal, 
+         --! output signals will be synchroized to this clock domain
+      reset_in :  in std_logic; --! Reset signal, ('1' = Reset)
+      sig_in   :  in letter;   --! Letter coming into the entity
+      sig_out  :  out letter   --! Partially encoded letter leaving entity
+	);
+
+end entity;
+
+architecture rtl of wheel is
+
+type wheel_variants is (1,2,3,4,5,6,7,8,beta,gamma);
+
+type wiring is array (wheel_variants) of array (letter) of letter;
+
+constant turnover is array (wheel_variants) of letter;
+	
+begin
+
+reflector: process(clk_in,reset_in)
+begin
+	if reset_in then
+		sig_out <= a;
+	elsif rising_edge(clk_in) then
+		sig_out <= (sig_in)
+	end if;
+	
+end process;
+
+
+end rtl;
